ef3be482ce822a72a945cdf8f78274fb6e6c8cea
Hounter139 <davidcr139@hotmail.com>
2020-10-28 10:33:31 -0400

    Css et integration html
    Changement css pour que le body fit et overlap pas
    Intégration de la page html wordpress dans le php

--- sites/cheminement.php
+++ sites/cheminement.php
@@ -1,7 +1,17 @@
+
 <?php
     $page = "cheminement";
 	require("../inclusions/header.inc.php");
 ?>
+
+<div class="cheminementrond">
+	<button class="rond">Image numérique</button>
+	<button class="rond">Programmation Web</button>
+	<button class="rond">Animation</button>
+	<button class="rond">Montage vidéo</button>
+	<button class="rond">3d</button>
+	<button class="rond">Montage Son</button>
+</div>
 
 <?php
 	require("../inclusions/footer.inc.php");
