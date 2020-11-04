e58e5a4c626a6dbcb17938154902b01b95c7948a
Xunrae <a_l040@outlook.com>
2020-11-04 09:37:23 -0500

    Cheminement
    Ajout affichage dynamique de tous les cours, js en cours

--- sites/cheminement.php
+++ sites/cheminement.php
@@ -3,6 +3,23 @@
 	require("../inclusions/header.inc.php");
 ?>
 
+<?php 
+	$fichier = "../json/Cheminement.json";
+
+	$tousCours = json_decode(file_get_contents($fichier), true);
+
+	foreach($tousCours as $session){
+?>
+	<div class="cheminementrond">
+
+		<?php foreach($session as $unCours){?>
+			<button class="rond"><?= $unCours["NomCours"]?></button>
+		<?php }?>
+		<hr>
+	</div>
+
+<?php }?>
+
 <?php
 	require("../inclusions/footer.inc.php");
 ?>	
