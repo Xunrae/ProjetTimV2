1e71150a1360291e3510b512c517a921b708394b
Xunrae <a_l040@outlook.com>
2020-11-05 18:59:13 -0500

    Interaction avec pastilles cheminement avancé
    Crée un div qui contient la description du cours, css de base à changer mais l'important y est.

--- sites/cheminement.php
+++ sites/cheminement.php
@@ -9,12 +9,16 @@
 
 	$tousCours = json_decode(file_get_contents($fichier), true);
 
+	$noSession = 0;
+
 	foreach($tousCours as $session){
+		$noSession++;
 ?>
 	<div class="cheminementrond">
 
-		<?php foreach($session as $unCours){?>
-			<button class="rond"><?= $unCours["NomCours"]?></button>
+		<?php foreach($session as $unCours){
+			?>
+			<button class="rond" data-session="<?= $noSession?>"><p><?= $unCours["NomCours"]?></p></button>
 		<?php }?>
 		<hr>
 	</div>
