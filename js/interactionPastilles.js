661c6dde5b1d8a79062efed75868d09596535249
SandrineCrys <crysandrine@gmail.com>
2020-11-11 14:42:13 -0500

    Background
    linear background (fini). Commencement de polygons animés

--- js/interactionPastilles.js
+++ js/interactionPastilles.js
@@ -3,8 +3,33 @@ let cours = document.getElementsByClassName("rond");
 nodes = Array.prototype.slice.call(cours,0);
 nodes.forEach(ajoutListener);
 
+for(let i = 1; i<=1; i++){
+    let triangle = document.getElementById("triangle");
+let length = triangle.getTotalLength();
+triangle.style.strokeDasharray = length;
+
+// Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
+triangle.style.strokeDashoffset = length;
+
+// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+window.addEventListener("scroll", myFunction);
+
+function myFunction() {
+let scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+  let draw = length * scrollpercent;
+  
+  // Reverse the drawing (when scrolling upwards)
+  triangle.style.strokeDashoffset = length - draw;
+}
+
+triangle.style.stroke = "black";
+triangle.style.position = "fixed";
+}
+console.log(triangle);
 function ajoutListener(){
     this.addEventListener('click',ouvrirFenetre,false);
+    
 }
 
 function ouvrirFenetre(){
@@ -12,4 +37,5 @@ function ouvrirFenetre(){
     let div =  document.createElement("div");
     let para = document.createElement("p");
     let contenu = document.createTextNode('<?= $fichier?>');
+    
 }
