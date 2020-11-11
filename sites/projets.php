ec7c449e31c12ca52f2ccd50ab69546da9bc2f31
LosTacosDeZelda <jeudyraphael@gmail.com>
2020-11-11 17:57:34 -0500

    Importé des vidéos pour 2 jeux dans projets
    Il y a des tooltip qui incitent l'utilisateur à cliquer sur les projets. Les titres des projets apparaissent maintenant avec la description et j'ai mis 2 vrais projets à la place de placeholders.

--- sites/projets.php
+++ sites/projets.php
@@ -3,45 +3,44 @@
 	require("../inclusions/header.inc.php");
 ?>
 <section>
-	<div class="projet">
-		<h2>Quercus</h2>
+	<div class="projet" title="Cliquez pour afficher la description">
+		<video src="../videos/QuercusFootage_Trim.mp4" muted autoplay loop preload="auto" aria-placeholder="ajshfdkjah"></video>
+		<div class="description">
+			<h2>Quercus</h2>
+			<p>Réalisé par des étudiants de 3ème année, dans le cadre du cours de Développement de Jeu. Quercus est un jeu d'aventure où l'on joue une petite créature de la forêt devant se défendre contre slimes et araignées dans les bois. C'est un topdown shooter, très amusant avec une ambiance de FOUUUU </p>
+		</div>
+	</div>
+	<div class="projet" title="Cliquez pour afficher la description">
+		<video src="../videos/FootageElementaris.mp4" muted autoplay loop preload="auto"></video>
+		<div class="description">
+			<h2>Elementaris</h2>
+			<p>Réalisé par des étudiants de 3ème année, dans le cadre du cours de Développement de Jeu. Elementaris est un "tower defense" où les tourelles sont basées sur les 4 éléments (eau,air,terre,feu). Elles ont des effets correspondants : le feu brûle, la glace ralentit,etc. Le but est de se débarasser des vagues d'ennemis, de manière à les annihiler COMPLÈTEMENT.</p>
+		</div>
+	</div>
+	<div class="projet" title="Cliquez pour afficher la description">
 		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
 		<div class="description">
 			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
 		</div>
 	</div>
-	<div class="projet">
-		<h2>Destination Bangkok</h2>
+	<div class="projet" title="Cliquez pour afficher la description">
 		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
 		<div class="description">
+			<h2>Destination Bangkok-Jeu</h2>
 			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
 		</div>
 	</div>
-	<div class="projet">
-		<h2>Destination Bangkok-Jeu</h2>
+	<div class="projet" title="Cliquez pour afficher la description">
 		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
 		<div class="description">
+			<h2>Destination Bangkok-Jeu</h2>
 			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
 		</div>
 	</div>
-	<div class="projet">
-		<h2>Destination Bangkok-Jeu</h2>
+	<div class="projet" title="Cliquez pour afficher la description">
 		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
 		<div class="description">
-			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
-		</div>
-	</div>
-	<div class="projet">
-		<h2>Destination Bangkok-Jeu</h2>
-		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
-		<div class="description">
-			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
-		</div>
-	</div>
-	<div class="projet">
-		<h2>Destination Bangkok-Jeu</h2>
-		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
-		<div class="description">
+			<h2>Destination Bangkok-Jeu</h2>
 			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
 		</div>
 	</div>
