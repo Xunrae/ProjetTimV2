6b90b3d83984f91d8749195d6d6a39b34a7065db
LosTacosDeZelda <jeudyraphael@gmail.com>
2020-11-11 18:07:32 -0500

    Fix URls Videos

--- sites/projets.php
+++ sites/projets.php
@@ -4,14 +4,14 @@
 ?>
 <section>
 	<div class="projet" title="Cliquez pour afficher la description">
-		<video src="../videos/QuercusFootage_Trim.mp4" muted autoplay loop preload="auto" aria-placeholder="ajshfdkjah"></video>
+		<video src="../videos/QuercusFootage.mp4" muted autoplay loop preload="auto" aria-placeholder="ajshfdkjah"></video>
 		<div class="description">
 			<h2>Quercus</h2>
 			<p>Réalisé par des étudiants de 3ème année, dans le cadre du cours de Développement de Jeu. Quercus est un jeu d'aventure où l'on joue une petite créature de la forêt devant se défendre contre slimes et araignées dans les bois. C'est un topdown shooter, très amusant avec une ambiance de FOUUUU </p>
 		</div>
 	</div>
 	<div class="projet" title="Cliquez pour afficher la description">
-		<video src="../videos/FootageElementaris.mp4" muted autoplay loop preload="auto"></video>
+		<video src="../videos/FootageElementaris_Trim.mp4" muted autoplay loop preload="auto"></video>
 		<div class="description">
 			<h2>Elementaris</h2>
 			<p>Réalisé par des étudiants de 3ème année, dans le cadre du cours de Développement de Jeu. Elementaris est un "tower defense" où les tourelles sont basées sur les 4 éléments (eau,air,terre,feu). Elles ont des effets correspondants : le feu brûle, la glace ralentit,etc. Le but est de se débarasser des vagues d'ennemis, de manière à les annihiler COMPLÈTEMENT.</p>
