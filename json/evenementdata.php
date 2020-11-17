97cecc1575570f54a508c38de24c86f968bb653a
Hounter139 <davidcr139@hotmail.com>
2020-11-17 16:07:06 -0500

    Evenement description

--- json/evenementdata.php
+++ json/evenementdata.php
@@ -1,38 +1,38 @@
 <?php
 
-$porteouverteautomne2020 = [
-    "Titre" => "Portes ouvertes automne 2020",
-    "Description" => "Ammenez vos enfants à ces portes ouvertes!"
+$ConcertParty = [
+    "Titre" => "Concert Party",
+    "Description" => "Soirée de performance musicale"
     
 ];
 
 $gamejam2021 = [
   
     "Titre" => "Gamejam 2021",
-    "Description" => "Le gamejam de 2021 est le meilleur endroit pour rekt des noobs..."
+    "Description" => "Le gamejam de 2021 est le meilleur endroit pour prouver ces connaissances en tant que developpeur de jeux"
 ];
 
 $porteouvertehiver2021 = [
     "Titre" => "Portes ouvertes hiver 2021",
-    "Description" => "Ammenez vos enfants à ces portes ouvertes!"
+    "Description" => "Journée d'information pour le collège en hiver 2021"
     
 ];
 
 $timtalk2021 = [
-    "Titre" => "timtalk2021",
-    "Description" => ""
+    "Titre" => "Tim talk 2021",
+    "Description" => "Tous les étudiants TIM (1ère, 2e et 3 année) sont invités pour un petit TIM-TALK. Cet événement vous permet de rencontrer 4 anciens finissants du programme TIM-Maisonneuve. Ils viennent vous parler de leur parcours professionnel depuis leur graduation en TIM.Alors, si vous avez besoin d'un peu de motivation, d'un petit pep-talk, d'inspiration ou juste de venir voir ce que ça devient un bon timien(ne), alors cet événement est le bon pour vous.Au plaisir de vous y voir en grand nombre!Les profs du TIM Voir moins"
     
 ];
 
 $retrouvaille = [
-    "Titre" => "retrouvaille",
-    "Description" => ""
+    "Titre" => "Retrouvaille",
+    "Description" => "Pour clore les festivités entourant le 50e anniversaire du Collège, les employés et étudiants, anciens et actuels, sont invités à un 6 à 9 Retrouvailles au jardin intérieur. "
     
 ];
 
 $timarcarde = [
-    "Titre" => "tim arcade",
-    "Description" => ""
+    "Titre" => "Tim arcade",
+    "Description" => "Les finissant(e)s TIM 2019 du Collège de Maisonneuve vous invitent à l'arcade de jeux vidéo et de présentation des projets Web. jeudi 20 décembre au Vivoir du Collège. 13h-16h30 (arcade) et 17h30-19h30 (sites Web)"
     
 ];
 
