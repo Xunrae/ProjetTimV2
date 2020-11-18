321c8a504708b0e2f5470236e90ac09889527c5a
LosTacosDeZelda <jeudyraphael@gmail.com>
2020-11-18 00:20:13 -0500

    Update Descriptions profs et cheminement
    Corrigé JSON, fait le css des descriptions
    cheminement + profs. Rajouté 1 projet

--- sites/projets.php
+++ sites/projets.php
@@ -4,7 +4,7 @@
 ?>
 <section>
 	<div class="projet" title="Cliquez pour afficher la description">
-		<video src="../videos/QuercusFootage.mp4" muted autoplay loop preload="auto" aria-placeholder="ajshfdkjah"></video>
+		<video src="../videos/QuercusFootage.mp4" muted autoplay loop preload="auto" ></video>
 		<div class="description">
 			<h2>Quercus</h2>
 			<p>Réalisé par des étudiants de 3ème année, dans le cadre du cours de Développement de Jeu. Quercus est un jeu de type « Shoot'em up » disponible exclusivement sur la platforme  PC. Le but du jeu est de se rendre à une destination précise tout en tirant avec des armes sur les masses d'adversaires qui veulent la peau du héros. Des obstacles variés figurent dans les divers niveaux qui se démarquent chacun par leur ambiance et leur thématique. </p>
@@ -32,17 +32,17 @@
 		</div>
 	</div>
 	<div class="projet" title="Cliquez pour afficher la description">
-		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
+		<img src="../img/projets/QuizAntoine.PNG"/>	
 		<div class="description">
-			<h2>Destination Bangkok-Jeu</h2>
-			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
+			<h2>Quiz-O-Matic, Antoine L'Écuyer</h2>
+			<p>Projet Web réalisé par un étudiant de 1ère année. Ce quiz interactif comporte des questions sur les jeux vidéos, des années 2000 à aujourd'hui. </p>
 		</div>
 	</div>
 	<div class="projet" title="Cliquez pour afficher la description">
-		<video src="../videos/EffetDeFuego.mp4" muted autoplay loop preload="auto"></video>
+		<video src="../videos/QuercusFootage.mp4" muted autoplay loop preload="auto" ></video>
 		<div class="description">
-			<h2>Destination Bangkok-Jeu</h2>
-			<p>Description blablalbabjklksdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkalkdjlkfjlkajfdlkajklfdjalk</p>
+			<h2>Développement de Jeu 2D</h2>
+			<p>Project Beast est un platformer 2D dont le but est de tuer les ennemis sans mourir pour arriver à la fin du niveau. </p>
 		</div>
 	</div>
 	
